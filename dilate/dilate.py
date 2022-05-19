bd261edee4202af19405d4d680d445d7cf3ac7bf
h3z <hzzz0823@gmail.com>
2022-05-19 11:13:17 +0800

    .

--- dilate/dilate.py
+++ dilate/dilate.py
@@ -9,6 +9,7 @@ from settings import FLOAT_TYPE
 
 alpha = 0.5
 # alpha = 0
+gamma = 0.001
 
 
 class TMP:
@@ -22,7 +23,7 @@ class TMP:
 
 
 @cuda.jit
-def loss_add(loss_shapes, paths, losses, omega):
+def loss_add(loss_shapes, paths, losses, omega, alpha):
     idx = cuda.grid(1)
     if idx >= len(loss_shapes):
         return
@@ -43,7 +44,7 @@ def loss_add(loss_shapes, paths, losses, omega):
 
 tmp = TMP()
 # def dilate_loss(outputs, targets, alpha=0.5, gamma=0.001):
-def dilate_loss(items, worker_n, alpha=0.5, gamma=0.001):
+def dilate_loss(items, worker_n):
     start = datetime.now()
     # outputs, targets: shape (batch_size, N_output, 1)
     # 按奇数处理，剩下的一个不要了。
@@ -74,12 +75,7 @@ def dilate_loss(items, worker_n, alpha=0.5, gamma=0.001):
         assert loss_shapes.shape[0] == L
 
         dloss = nb.cuda.device_array(shape=(L), dtype=FLOAT_TYPE)
-        loss_add[L // 512 + 1, 512](
-            loss_shapes,
-            paths,
-            dloss,
-            domega,
-        )
+        loss_add[L // 512 + 1, 512](loss_shapes, paths, dloss, domega, alpha)
         losses_rows = dloss.copy_to_host()
 
         for j in range(L - row - 1):
@@ -91,3 +87,35 @@ def dilate_loss(items, worker_n, alpha=0.5, gamma=0.001):
 
     # pickle.dump(losses, open(f"losses_0517_{worker_n}.pkl", "wb"))
     return losses
+
+
+def dilate_loss_one(items, row, alpha):
+    items = items[:-1]
+    L = len(items)
+    N = len(items[0])
+
+    losses = np.zeros((len(items), len(items)))
+    omega = np.array(range(1, N + 1)).reshape(N, 1)
+    omega = soft_dtw.pairwise_distances(omega)
+    domega = cuda.to_device(omega)
+
+    items = np.array(items).astype(FLOAT_TYPE)
+    ditems = nb.cuda.to_device(items)
+
+    paths = path_soft_dtw.compute_dilate_path(gamma, ditems, row)
+    assert paths.shape[0] == L
+
+    loss_shapes = soft_dtw.compute_soft_dtw_batch(gamma, ditems, row)
+    assert loss_shapes.shape[0] == L
+
+    dloss = nb.cuda.device_array(shape=(L), dtype=FLOAT_TYPE)
+    loss_add[L // 512 + 1, 512](loss_shapes, paths, dloss, domega, alpha)
+
+    losses_rows = dloss.copy_to_host()
+
+    for j in range(L - row - 1):
+        losses[row, j + row + 1] = losses_rows[j]
+    for j in range(L - row - 1, L):
+        losses[L - row - 2, j] = losses_rows[j]
+
+    return losses[row]
